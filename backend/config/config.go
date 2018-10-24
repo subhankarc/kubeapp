a5e491fe2f7df29790a755e5f60094f82ae0751a
Sushant Mahajan <sushant.mahajan@sap.com>
2018-10-24 11:16:50 +0530

    parse config from env

--- backend/config/config.go
+++ backend/config/config.go
@@ -1,10 +1,8 @@
 package config
 
 import (
-	"encoding/json"
 	"log"
 	"os"
-	"strings"
 
 	"github.com/smjn/ipl18/backend/models"
 )
@@ -21,14 +19,31 @@ var GetDBConfig = func() models.DBConfig {
 
 func init() {
 	log.Println("Parsing config from env...")
-	if os.Getenv("app_config") != "" {
-		if err := json.NewDecoder(strings.NewReader(os.Getenv("app_config"))).Decode(&serverConfig); err != nil {
-			log.Println("error parsing config", err.Error())
-			os.Exit(1)
-		}
+
+	if os.Getenv("POSTGRES_DB") != "" {
+		serverConfig.DB.DBName = os.Getenv("POSTGRES_DB")
 	} else {
-		log.Println("Config: app_config not found in env, exiting")
+		log.Println("error parsing config")
 		os.Exit(1)
 	}
+	if os.Getenv("POSTGRES_USER") != "" {
+		serverConfig.DB.DBUser = os.Getenv("POSTGRES_USER")
+	} else {
+		log.Println("error parsing config")
+		os.Exit(1)
+	}
+	if os.Getenv("POSTGRES_PASSWORD") != "" {
+		serverConfig.DB.DBPassword = os.Getenv("POSTGRES_PASSWORD")
+	} else {
+		log.Println("error parsing config")
+		os.Exit(1)
+	}
+	if os.Getenv("POSTGRES_HOST") != "" {
+		serverConfig.DB.Host = os.Getenv("POSTGRES_HOST")
+	} else {
+		log.Println("error parsing config")
+		os.Exit(1)
+	}
+	serverConfig.DB.Port = "5432"
 	log.Println(serverConfig)
 }
